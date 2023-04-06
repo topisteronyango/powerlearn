f5bf833e231122d317810cebe4953f0a1370bf2e
t-onyango <t.onyango@alustudent.com>
2023-04-06 14:38:49 +0200

    power learn project

--- kezia/JS/fin.js
+++
@@ -1,9 +0,0 @@
-const passwordInput = document.querySelector("#password")
-const eye = document.querySelector("#eye")
-
-eye.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash")
-    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
-    passwordInput.setAttribute("type", type)
-  })
-  
